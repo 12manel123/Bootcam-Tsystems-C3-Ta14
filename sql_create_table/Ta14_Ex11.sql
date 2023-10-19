3753a9d9b27eadf4cd6fd8ee60edb0e5e45f94d3
Manel Castellví Cerezuela <manelcastellvicerezuela@gmail.com>
2023-10-19 15:39:24 +0200

    Update Ta14_Ex11.sql

--- sql_create_table/Ta14_Ex11.sql
+++ sql_create_table/Ta14_Ex11.sql
@@ -2,31 +2,30 @@ create database Investigadores;
 use Investigadores;
 
 CREATE TABLE Facultad (
-    codigo int PRIMARY KEY,
-    nombre nvarchar(100) NOT NULL
+    codigo INT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE Investigadores (
-    DNI varchar(8) PRIMARY KEY,
-    nomApels nvarchar(255) NOT NULL,
-    facultad int NOT NULL,
+    DNI VARCHAR(8) PRIMARY KEY,
+    nomApels VARCHAR(255) NOT NULL,
+    facultad INT NOT NULL,
     FOREIGN KEY (facultad) REFERENCES Facultad(codigo)
 );
 
 CREATE TABLE Equipos (
-    numSerie char(4) PRIMARY KEY,
-    nombre nvarchar(100) NOT NULL,
-    comienzo datetime NOT NULL,
-    fin datetime NOT NULL,
-    facultad int NOT NULL,
+    numSerie CHAR(4) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    comienzo DATE NOT NULL,
+    fin DATE NOT NULL,
+    facultad INT NOT NULL,
     FOREIGN KEY (facultad) REFERENCES Facultad(codigo)
 );
 
 CREATE TABLE Reserva (
-    DNI varchar(8) NOT NULL,
-    numSerie char(4) NOT NULL,
+    DNI VARCHAR(8) NOT NULL,
+    numSerie CHAR(4) NOT NULL,
     PRIMARY KEY (DNI, numSerie),
     FOREIGN KEY (DNI) REFERENCES Investigadores(DNI) ON DELETE CASCADE,
     FOREIGN KEY (numSerie) REFERENCES Equipos(numSerie) ON DELETE CASCADE
 );
-
